4810b0ec4d106688ad84cdd95976bf1a77484615
rdw100 <ryandustinwright@hotmail.com>
2021-12-08 11:07:42 -0500

    Added WASM.  Added Cors.  Configured DI Service.  Upated Grpc.Net.Client.

--- Fsp.Grpc.server/Program.cs
+++ Fsp.Grpc.server/Program.cs
@@ -1,17 +1,36 @@
 using Fsp.Grpc.Server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddGrpc();
+builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder =>
+{
+    builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader()
+           .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+}));
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
 builder.Services.AddGrpc();
+//builder.Services.AddControllersWithViews();
+//builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
+app.UseRouting();
+app.UseGrpcWeb();
+app.UseCors();
+
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGrpcService<GreeterService>().EnableGrpcWeb()
+                                                .RequireCors("AllowAll");
+});
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
