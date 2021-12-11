dd10522fcd61f26c0f1f55286e027507d284f646
rdw100 <ryandustinwright@hotmail.com>
2021-12-11 18:22:51 -0500

    Added Parks Service.  Added Protos files for both client and server.

---
+++ Fsp.Grpc.server/Services/ParksService.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Core;
+
+namespace Fsp.Grpc.Api.Services
+{
+    public class ParksService : Parks.ParksBase
+    {
+        private readonly ILogger<ParksService> _logger;
+
+        public ParksService(ILogger<ParksService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<ParksResponse> GetParks(ParksRequest request, ServerCallContext context)
+        {
+            ParksResponse result = new ParksResponse();
+            result.MyParks.Add(new Park { Id = 1, Name = "Addison Blockhouse Historic State Park", County = "Volusia", Size = "134.51 acres (54.43 ha)", Year = "1939", Water = "Tomoka River", Remarks = "Ruins of a 19th-century plantation owned by John Addison" });
+            result.MyParks.Add(new Park { Id = 2, Name = "South Point", County = "Hillsborough", Size = "6,312 acres (2,556 ha)", Year = "1996", Water = "Alafia River", Remarks = "Former phosphorus strip mine unremediated" });
+
+            return Task.FromResult(result);
+        }
+    }
+}
