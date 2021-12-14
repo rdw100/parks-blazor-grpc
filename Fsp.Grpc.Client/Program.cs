5ad1f62c04914061e11c577ea3fdbbebac7e77cd
rdw100 <ryandustinwright@hotmail.com>
2021-12-14 11:21:44 -0500

    Added parks service call to console app.  Added ConsoleTables for display.

--- Fsp.Grpc.Client/Program.cs
+++ Fsp.Grpc.Client/Program.cs
@@ -1,12 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Grpc.Net.Client;
+using ConsoleTables;
 using Fsp.Grpc.Api;
+using Grpc.Net.Client;
 
 // The port number must match the port of the gRPC server.
 using var channel = GrpcChannel.ForAddress("https://localhost:7217");
 var client = new Greeter.GreeterClient(channel);
 var reply = await client.SayHelloAsync(
                   new HelloRequest { Name = "GreeterClient" });
-Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Greeting: " + reply.Message + "\n");
+
+// Request list of parks from grpc service
+var parkClient = new Visitor.VisitorClient(channel);
+var parkResponse = await parkClient.GetParksAsync(
+                  new ParksRequest { });
+Console.WriteLine("Enjoy Florida State Parks that start with \"A\" ...");
+parkResponse.Parks.ToList().ForEach(i => Console.WriteLine(i + ", "));
+
+// Format grpc service response list data using ConsoleTables 
+Console.WriteLine("\n");
+ConsoleTable.From(parkResponse.Parks.Select(x => new { x.Name, x.County, x.Size }).ToList()).Write();
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
