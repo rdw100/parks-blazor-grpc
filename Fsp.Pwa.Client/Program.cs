f1a46aacaad8e3c5418896da5f32c3964c06b7f5
rdw100 <ryandustinwright@hotmail.com>
2021-12-14 08:27:49 -0500

    Added parks component to PWA.

--- Fsp.Pwa.Client/Program.cs
+++ Fsp.Pwa.Client/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 
 builder.Services.AddSingleton(services =>
 {
-    // Grpc Service URI
+    // Grpc Service URI (normally, appsettings.json)
     var backendUrl = "https://localhost:7217";
 
     // Create a gRPC-Web channel pointing to the backend server
@@ -24,6 +24,20 @@ builder.Services.AddSingleton(services =>
 
     // Now we can instantiate gRPC clients for this channel
     return new Greeter.GreeterClient(channel);
+});
+
+//Add gRPC service
+builder.Services.AddSingleton(services =>
+{
+    // Grpc Service URI (normally, appsettings.json)
+    var backendUrl = "https://localhost:7217";
+
+    // Create a gRPC-Web channel pointing to the backend server
+    var httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
+    var channel = GrpcChannel.ForAddress(backendUrl, new GrpcChannelOptions { HttpClient = httpClient });
+
+    // Now we can instantiate gRPC clients for this channel
+    return new Visitor.VisitorClient(channel);
 });
 
 await builder.Build().RunAsync();
